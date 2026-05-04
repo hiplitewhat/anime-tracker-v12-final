640c5b13fee9a7be1f3ec91e190778e5ccdacff0
hiplitewhat <hiplitewhat@gmail.com>
2026-05-04 21:33:34 +0800

    fix: enforce executable permissions for gradlew

---
+++ app/src/main/java/com/animetracker/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.animetracker
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity : AppCompatActivity() { override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState) } }
